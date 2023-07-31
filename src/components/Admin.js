4c908214d5be57b025dd8049ca635e5f2e8a870e
unknown <akepatiakashreddy@gmail.com>
2023-07-31 19:29:47 +0530

    auth css change

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -376,7 +376,7 @@ const Admin = () => {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">{nam==""?"Add Product":"Edit Product"}</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -449,7 +449,7 @@ const Admin = () => {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Order Details</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
